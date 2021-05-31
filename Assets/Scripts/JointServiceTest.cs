1768015e094719d60d7196ad31bb17c942aa227a
glannuzel <glannuzel@ensc.fr>
2021-05-31 17:00:52 +0200

    Fix uid and add streams.

--- Assets/Scripts/JointServiceTest.cs
+++ Assets/Scripts/JointServiceTest.cs
@@ -9,6 +9,7 @@ using Reachy;
 using Grpc.Core;
 using Grpc.Core.Utils;
 using Reachy.Sdk.Joint;
+using Reachy.Sdk.Fan;
 
 class JointServiceTest : MonoBehaviour
 {
@@ -37,7 +38,10 @@ class JointServiceTest : MonoBehaviour
         const int PortJoint = 50055;
         Server server = new Server
         {
-            Services = { JointService.BindService(new JointServiceImpl()), SensorService.BindService(new SensorServiceImpl()) },
+            Services = { 
+                JointService.BindService(new JointServiceImpl()), 
+                SensorService.BindService(new SensorServiceImpl()),
+                FanControllerService.BindService(new FanControllerServiceImpl()) },
             Ports = { new ServerPort("localhost", PortJoint, ServerCredentials.Insecure) }
         };
         server.Start();
@@ -70,6 +74,21 @@ class JointServiceTest : MonoBehaviour
             }
         }
 
+        // public override Task<JointsCommandAck> StreamJointsCommands(IAsyncStreamReader<JointsCommand> requestStream, ServerCallContext context)
+        // {
+        //     await foreach (var message in requestStream.ReadAllAsync())
+        //     {
+        //         Dictionary<JointId, float> commands = new Dictionary<JointId, float>();
+        //         for(int i=0; i<jointsCommand.Commands.Count; i++)
+        //         {
+        //             float command = Mathf.Rad2Deg * (float)jointsCommand.Commands[i].GoalPosition;
+        //             commands.Add(jointsCommand.Commands[i].Id, command);
+        //         }
+        //         reachy.HandleCommand(commands);
+        //     }
+        //     return (new JointsCommandAck { Success = false });
+        // }
+
         public override Task<JointsState> GetJointsState(JointsStateRequest jointRequest, ServerCallContext context)
         {
             Dictionary<JointId, JointField> request = new Dictionary<JointId, JointField>();
@@ -85,18 +104,51 @@ class JointServiceTest : MonoBehaviour
             {
                 var jointState = new JointState();
                 jointState.Name = item.name;
+                jointState.Uid = (uint?)item.uid;
                 jointState.PresentPosition = item.present_position;
 
                 listJointStates.Add(jointState);
                 listJointIds.Add(new JointId { Name = item.name });
             };
 
+            JointsState state = new JointsState {
+                Ids = { listJointIds },
+                States = { listJointStates },
+            };
+
+            return Task.FromResult(state);
+        }
+
+        public override async Task StreamJointsState(StreamJointsRequest jointRequest, Grpc.Core.IServerStreamWriter<JointsState> responseStream, ServerCallContext context)
+        {
+            while (!context.CancellationToken.IsCancellationRequested)
+            {
+                Dictionary<JointId, JointField> request = new Dictionary<JointId, JointField>();
+                for(int i=0; i<jointRequest.Request.Ids.Count; i++)
+                {
+                    request.Add(jointRequest.Request.Ids[i], JointField.PresentPosition);
+                }
+                var motors = reachy.GetCurrentMotorsState(request);
+                
+                List<JointState> listJointStates = new List<JointState>();
+                List<JointId> listJointIds = new List<JointId>();
+                foreach (var item in motors)
+                {
+                    var jointState = new JointState();
+                    jointState.Name = item.name;
+                    jointState.PresentPosition = item.present_position;
+
+                    listJointStates.Add(jointState);
+                    listJointIds.Add(new JointId { Name = item.name });
+                };
+
                 JointsState state = new JointsState {
                     Ids = { listJointIds },
                     States = { listJointStates },
                 };
-
-            return Task.FromResult(state);
+                await responseStream.WriteAsync(state);
+                await Task.Delay(TimeSpan.FromSeconds(1), context.CancellationToken);
+            }
         }
 
         public override Task<JointsId> GetAllJointsId(Google.Protobuf.WellKnownTypes.Empty empty, ServerCallContext context)
@@ -143,8 +195,6 @@ class JointServiceTest : MonoBehaviour
         public override Task<SensorsState> GetSensorsState(SensorsStateRequest stateRequest, ServerCallContext context)
         {
             var sensors = reachy.GetCurrentSensorsState(stateRequest.Ids);
-
-            Debug.Log(sensors);
             
             List<SensorState> listSensorStates = new List<SensorState>();
             List<SensorId> listSensorIds = new List<SensorId>();
