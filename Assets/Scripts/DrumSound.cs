586bd8a3d6f4e97f9b3ea3311a503acd3737d838
Gaëlle Lannuzel <glannuzel@ensc.fr>
2022-05-06 16:47:00 +0200

    Position elements and add sound on collision enter.

---
+++ Assets/Scripts/DrumSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrumSound : MonoBehaviour
+{
+    public AudioClip sound;
+
+    void Start()
+    {
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = sound;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Left Wrist Roll" || collision.gameObject.name == "Right Wrist Roll")
+        {
+            Debug.Log("Play sound");
+            GetComponent<AudioSource>().PlayOneShot(sound, 0.7F);;
+        }
+    }
+}
---
+++ Assets/Scripts/DrumsButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrumsButton : MonoBehaviour
+{
+    public Transform drums;
+    public Button drumsButton;
+
+    public void Start()
+    {
+        drumsButton.onClick.AddListener(DisplayDrums);
+    }
+    
+    public void DisplayDrums()
+    {
+        Debug.Log("Display drums");
+        drums.gameObject.SetActive(true);
+    }
+
+    public void HideDrums()
+    {
+        Debug.Log("Hide drums");
+        drums.gameObject.SetActive(false);
+    }
+}
